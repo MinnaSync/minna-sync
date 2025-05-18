e11e4f18dd4c6c4608d8b2f118d94a7d27c252c4
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-05-18 14:25:36 -0400

    feat: buttons

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,46 @@
+import { JSX } from 'react';
+
+import { Typography } from '#/components/Typography/Typography';
+import styles from './Button.module.scss'
+
+interface ButtonProps {
+    disabled?: boolean;
+    color: 'Primary' | 'White' | 'Danger' | 'Warning' | 'Positive' | 'Neutral';
+    display: 'Filled' | 'Outlined' | 'Link' | 'Ghost';
+    textInverted?: boolean;
+    icon?: {
+        position: 'left' | 'right';
+        element: JSX.Element;
+    };
+    children?: string;
+    onClick?: () => void;
+}
+
+export default function Button(
+    props: ButtonProps
+) {
+    const color = styles[`color${props.color}`];
+    const type = styles[`display${props.display}`];
+
+    return (
+        <button
+            disabled={props.disabled ? true : undefined}
+            className={
+                `${color} ${type}
+                ${props.textInverted ? `${styles.textColorInverted}` : ''}
+                ${props.icon && !props.children ? `${styles.iconOnly}` : ''}`
+            }
+            onClick={
+                props.disabled ? undefined : props.onClick
+            }
+        >
+            { props.icon && props.icon.position === 'left' && props.icon.element }
+            { props.children &&
+                <Typography variant="heading" weight="bold">
+                    {props.children}
+                </Typography>
+            }
+            { props.icon && props.icon.position === 'right' && props.icon.element }
+        </button>
+    )
+}
