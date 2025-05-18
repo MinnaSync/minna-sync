ccf8db68562db22d7e4dc57f38bac5d41c3d05ec
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-05-17 21:35:11 -0400

    refactor: websocket types

--- src/util/ws/types.ts
+++ src/util/ws/types.ts
@@ -9,20 +9,67 @@ export type MessageEvent = {
     data: string | number | object;
 };
 
-export type UserMessageEvent = {
+type GenericUser = {
+    /**
+     * The username for the user.
+     */
     username: string;
+}
+
+export type UserJoinEvent = GenericUser;
+
+export type UserLeftEvent = GenericUser;
+
+export interface UserMessageEvent extends GenericUser {
+    /**
+     * The message that the user sent.
+     */
     message: string;
 };
 
-export type UserJoinEvent = {
-    username: string;
-};
-
-export type UserLeftEvent = {
-    username: string;
-};
-
-export type MediaUpdateEvent = {
+export type GenericMedia = {
+    /**
+     * The title of the currently playing media.
+     */
     title: string;
+    /**
+     * The series of the currently playing media.
+     */
+    series: string;
+    /**
+     * The URL associated with the media.
+     */
     url: string;
+    /**
+     * The poster image of the anime series.
+     */
+    poster_image_url: string;
 }
+
+export type GenericPlayingMedia = {
+    /**
+     * Whether or not the current media is played or paused.
+     */
+    paused: boolean;
+    /**
+     * The current playback time of the media.
+     */
+    current_time: number;
+}
+
+export interface NowPlaying extends GenericMedia, GenericPlayingMedia {};
+
+export type MediaUpdateEvent = GenericMedia;
+
+export type RoomDataEvent = {
+    /**
+     * The current playing media.
+     */
+    now_playing: NowPlaying;
+    /**
+     * An array of media that is queued to be played.
+     */
+    queue: Array<MediaUpdateEvent>;
+};
+
+export type TimeUpdateEvent = GenericPlayingMedia;
