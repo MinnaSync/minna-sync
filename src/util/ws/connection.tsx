24c231a76fd673a678ca0cb5be336cf0c50e447d
LuckFire <39076891+LuckFire@users.noreply.github.com>
2025-05-25 15:45:52 -0400

    doc: document websocket class

--- src/util/ws/connection.tsx
+++ src/util/ws/connection.tsx
@@ -9,6 +9,14 @@ type EmitOptions = {
     queue?: boolean;
 }
 
+type ListenerOptions = {
+    /**
+     * A custom abort signal to use to abort event listeners.
+     * This will automatically call off() with the callback to remove the specific event.
+     */
+    signal?: AbortSignal;
+}
+
 export class Websocket {
     private url: string;
     private ws: WebSocket | undefined;
@@ -22,6 +30,11 @@ export class Websocket {
     private readonly controller: AbortController;
     private readonly signal: AbortSignal;
 
+    /**
+     * A custom websocket implementation that handles the server's responses.
+     * Inspired off of socket.io's way of handling event.
+     * @param url The ws/wss connection url.
+     */
     constructor(url: string) {
         this.url = url;
         this.connected = false;
@@ -33,6 +46,9 @@ export class Websocket {
         this.signal = this.controller.signal;
     }
 
+    /**
+     * Create a connection to the websocket.
+     */
     public connect() {
         if (this.connected || this.ws?.readyState === WebSocket.OPEN) {
             return this;
@@ -82,7 +98,13 @@ export class Websocket {
         return this;
     }
 
-    public reconnect(attempt: number = 0) {
+    /**
+     * Attempt to reconnect to the websocket.
+     * 
+     * This method is only used by the class itself.
+     * If an attempt at reconnection wants to be maade, use the connect() method.
+     */
+    private reconnect(attempt: number = 0) {
         const maxAttempts = 5;
         if (attempt >= maxAttempts || this.connected || this.reconnecting) return;
 
@@ -95,6 +117,9 @@ export class Websocket {
         }, 1_000 * attempt);
     }
 
+    /**
+     * Disconnect from the websocket.
+     */
     public disconnect() {
         this.ws?.close();
 
@@ -102,29 +127,46 @@ export class Websocket {
         this.reconnecting = false;
     }
 
-    public on(event: string, callback: (data: any) => void, opts?: { signal: AbortSignal }) {
+    /**
+     * Listen for an event from the server.
+     * @param event The event to listen for.
+     * @param callback The callback for when the event is emitted.
+     * @param {ListenerOptions} opts Extra options.
+     */
+    public on(event: string, callback: (data: any) => void, opts?: ListenerOptions) {
         if (!this.handlers.has(event)) {
             this.handlers.set(event, new Set());
         }
 
         this.handlers.get(event)?.add(callback);
 
-        opts?.signal.addEventListener("abort", () => {
+        opts?.signal?.addEventListener("abort", () => {
             this.off(event, callback);
         }, { once: true });
     }
 
-    public once(event: string, callback: (data: any) => void, opts?: { signal: AbortSignal }) {
+    /**
+     * Listens for an event once
+     * @param event The event to listen for.
+     * @param callback The callback for when the event is emitted.
+     * @param {ListenerOptions} opts Extra options.
+     */
+    public once(event: string, callback: (data: any) => void, opts?: ListenerOptions) {
         this.on(event, (data: any) => {
             this.off(event, callback);
             callback(data);
         });
-        
-        opts?.signal.addEventListener("abort", () => {
+
+        opts?.signal?.addEventListener("abort", () => {
             this.off(event, callback);
         }, { once: true });
     }
 
+    /**
+     * Stop listening to an event.
+     * @param event The event to stop listening for.
+     * @param callback The specific callback of the event.
+     */
     public off(event: string, callback?: (data: any) => void) {
         if (callback) {
             this.handlers.get(event)?.delete(callback);
@@ -136,9 +178,15 @@ export class Websocket {
             return;
         }
 
-        this.handlers.delete(event);
+        this.handlers.get(event)?.clear();
     }
 
+    /**
+     * Emit an event to the server.
+     * @param event The event to emit.
+     * @param data The data that needs to be sent.
+     * @param {EmitOptions} options Extra options.
+     */
     public emit(event: string, data: any, options?: EmitOptions) {
         if (this.ws?.readyState !== this.ws?.OPEN) {
             if (!options?.queue) return;
